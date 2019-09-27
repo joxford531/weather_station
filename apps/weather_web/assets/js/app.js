53296ac90fa00a9e117e642b78999881333b85bb
John Oxford <joxford531@gmail.com>
2019-09-27 21:26:39 +0000

    Update weather live view to use hooks so that window object isn't required

--- apps/weather_web/assets/js/app.js
+++ apps/weather_web/assets/js/app.js
@@ -18,5 +18,86 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+let myLine = null;
+
+let chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)'
+};
+
+let config = {
+  type: 'line',
+  data: {
+    datasets: [
+      {
+        label: 'BMP180 Temp',
+        borderColor: chartColors.red,
+        fill: false,
+        data: []
+      },
+      {
+        label: 'SHT31 Temp',
+        borderColor: chartColors.blue,
+        fill: false,
+        data: []
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Chart.js Time Point Data'
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Date'
+        },
+        ticks: {
+          major: {
+            fontStyle: 'bold',
+            fontColor: '#FF0000'
+          }
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Temp F'
+        }
+      }]
+    }
+  }
+};
+
+let hooks = {
+  chart: {
+    mounted() {
+      let ctx = document.getElementById('canvas').getContext('2d');
+
+      config.data.datasets[0].data = JSON.parse(this.el.dataset.bmp);
+      config.data.datasets[1].data = JSON.parse(this.el.dataset.sht);
+
+      myLine = new Chart(ctx, config);
+    },
+    updated() {
+      let el = document.getElementById("canvas-holder")
+      config.data.datasets[0].data = JSON.parse(el.dataset.bmp);
+      config.data.datasets[1].data = JSON.parse(el.dataset.sht);
+      myLine.update();
+    }
+  }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, { hooks })
 liveSocket.connect()
--- apps/weather_web/lib/weather_web/live/weather_daily.ex
+++ apps/weather_web/lib/weather_web/live/weather_daily.ex
@@ -5,97 +5,15 @@ defmodule WeatherWeb.WeatherDaily do
   def render(assigns) do
     ~L"""
     <div id="canvas-holder" data-bmp="<%= Jason.encode!(@bmp_data) %>"
-    data-sht="<%= Jason.encode!(@sht_data) %>" phx-hook="chart">
+      data-sht="<%= Jason.encode!(@sht_data) %>" phx-hook="chart">
       <canvas id="canvas" phx-update="ignore"></canvas>
     </div>
-    <script>
-      let myLine = null;
-
-      let chartColors = {
-        red: 'rgb(255, 99, 132)',
-        orange: 'rgb(255, 159, 64)',
-        yellow: 'rgb(255, 205, 86)',
-        green: 'rgb(75, 192, 192)',
-        blue: 'rgb(54, 162, 235)',
-        purple: 'rgb(153, 102, 255)',
-        grey: 'rgb(201, 203, 207)'
-      };
-
-      config = {
-        type: 'line',
-        data: {
-          datasets: [
-            {
-              label: 'BMP180 Temp',
-              borderColor: chartColors.red,
-              fill: false,
-              data: <%= Phoenix.HTML.raw Jason.encode!(@bmp_data) %>
-            },
-            {
-              label: 'SHT31 Temp',
-              borderColor: chartColors.blue,
-              fill: false,
-              data: <%= Phoenix.HTML.raw Jason.encode!(@sht_data) %>
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          title: {
-            display: true,
-            text: 'Chart.js Time Point Data'
-          },
-          scales: {
-            xAxes: [{
-              type: 'time',
-              display: true,
-              scaleLabel: {
-                display: true,
-                labelString: 'Date'
-              },
-              ticks: {
-                major: {
-                  fontStyle: 'bold',
-                  fontColor: '#FF0000'
-                }
-              }
-            }],
-            yAxes: [{
-              display: true,
-              scaleLabel: {
-                display: true,
-                labelString: 'Temp F'
-              }
-            }]
-          }
-        }
-      };
-
-      window.onload = () => {
-        let ctx = document.getElementById('canvas').getContext('2d');
-        myLine = new Chart(ctx, config);
-
-        setInterval(() => {
-          updateData();
-        }, 3000)
-      };
-
-      updateData = () => {
-        let el = document.getElementById("canvas-holder")
-        config.data.datasets[0].data = JSON.parse(el.dataset.bmp);
-        config.data.datasets[1].data = JSON.parse(el.dataset.sht);
-
-        console.log(config.data.datasets[0].data[config.data.datasets[0].data.length - 1])
-
-        myLine.update();
-      }
-    </script>
 
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(3000, self(), :tick)
+    if connected?(socket), do: :timer.send_interval(30000, self(), :tick)
 
     {:ok, put_hourly_data(socket)}
   end
