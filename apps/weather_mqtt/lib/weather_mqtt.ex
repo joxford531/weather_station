2c7fff5a031759d77753dc38b3e37e35b36901d0
John Oxford <joxford531@gmail.com>
2019-09-27 15:46:33 +0000

    Add new query for hourly data and rework liveview to auto-update without having to use hooks

--- apps/weather_mqtt/lib/weather_mqtt.ex
+++ apps/weather_mqtt/lib/weather_mqtt.ex
@@ -5,11 +5,39 @@ defmodule WeatherMqtt do
   alias WeatherMqtt.{History}
 
 
-  def get_between_time(start_time, end_time) do
+  def get_history_between(start_time, end_time) do
     Repo.all(
       from h in History,
       where: h.time >= ^start_time and h.time < ^end_time,
       order_by: [asc: h.time]
     )
+  end
+
+  def get_temps_between_raw(start_time, end_time) do
+    query = """
+    SELECT
+      ROUND(CAST(AVG(bmp_temp) as NUMERIC), 2) as bmp_temp,
+      ROUND(CAST(AVG(sht_temp) as NUMERIC), 2) as sht_temp,
+      to_timestamp(
+        floor(
+          (
+            extract(
+              'epoch'
+                from
+                time
+              ) / 600
+          )
+        ) * 600
+      ) as time_period
+    FROM
+      history
+    WHERE
+	    time >= $1 AND time <= $2
+    GROUP BY
+      time_period
+    ORDER BY
+      time_period asc;
+    """
+    Ecto.Adapters.SQL.query!(Repo, query, [start_time, end_time])
   end
 end
--- apps/weather_web/lib/weather_web/live/weather_daily.ex
+++ apps/weather_web/lib/weather_web/live/weather_daily.ex
@@ -4,11 +4,14 @@ defmodule WeatherWeb.WeatherDaily do
 
   def render(assigns) do
     ~L"""
-    <div style="width:75%;">
+    <div id="canvas-holder" data-bmp="<%= Jason.encode!(@bmp_data) %>"
+    data-sht="<%= Jason.encode!(@sht_data) %>" phx-hook="chart">
       <canvas id="canvas" phx-update="ignore"></canvas>
     </div>
     <script>
-      window.chartColors = {
+      let myLine = null;
+
+      let chartColors = {
         red: 'rgb(255, 99, 132)',
         orange: 'rgb(255, 159, 64)',
         yellow: 'rgb(255, 205, 86)',
@@ -18,15 +21,23 @@ defmodule WeatherWeb.WeatherDaily do
         grey: 'rgb(201, 203, 207)'
       };
 
-      var config = {
+      config = {
         type: 'line',
         data: {
-          datasets: [{
-            label: 'Dataset with string point data',
-            borderColor: window.chartColors.red,
-            fill: false,
-            data: <%= Phoenix.HTML.raw Jason.encode!(@data) %>
-          }]
+          datasets: [
+            {
+              label: 'BMP180 Temp',
+              borderColor: chartColors.red,
+              fill: false,
+              data: <%= Phoenix.HTML.raw Jason.encode!(@bmp_data) %>
+            },
+            {
+              label: 'SHT31 Temp',
+              borderColor: chartColors.blue,
+              fill: false,
+              data: <%= Phoenix.HTML.raw Jason.encode!(@sht_data) %>
+            }
+          ]
         },
         options: {
           responsive: true,
@@ -53,65 +64,115 @@ defmodule WeatherWeb.WeatherDaily do
               display: true,
               scaleLabel: {
                 display: true,
-                labelString: 'value'
+                labelString: 'Temp F'
               }
             }]
           }
         }
       };
-      window.onload = function() {
-        var ctx = document.getElementById('canvas').getContext('2d');
-        window.myLine = new Chart(ctx, config);
+
+      window.onload = () => {
+        let ctx = document.getElementById('canvas').getContext('2d');
+        myLine = new Chart(ctx, config);
+
+        setInterval(() => {
+          updateData();
+        }, 3000)
       };
+
+      updateData = () => {
+        let el = document.getElementById("canvas-holder")
+        config.data.datasets[0].data = JSON.parse(el.dataset.bmp);
+        config.data.datasets[1].data = JSON.parse(el.dataset.sht);
+
+        console.log(config.data.datasets[0].data[config.data.datasets[0].data.length - 1])
+
+        myLine.update();
+      }
     </script>
 
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(60000, self(), :tick)
+    if connected?(socket), do: :timer.send_interval(3000, self(), :tick)
 
-    {:ok, put_data(socket)}
+    {:ok, put_hourly_data(socket)}
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, put_data(socket)}
+    {:noreply, put_hourly_data(socket)}
   end
 
-  def test() do
+  defp put_daily_data(socket) do
     start_time =
       Timex.now("America/New_York")
       |> Timex.beginning_of_day()
+      |> Timex.Timezone.convert("Etc/UTC")
 
     end_time =
       Timex.now("America/New_York")
       |> Timex.end_of_day()
+      |> Timex.Timezone.convert("Etc/UTC")
 
-    data_points = WeatherMqtt.get_between_time(start_time, end_time)
+    {bmp_temps, sht_temps} =
+      WeatherMqtt.get_temps_between_raw(start_time, end_time)
+      |> Map.get(:rows)
+      |> format_raw_results()
 
-    format_temp_data(data_points)
+    assign(socket, bmp_data: bmp_temps, sht_data: sht_temps)
   end
 
-  defp put_data(socket) do
+  defp put_hourly_data(socket) do
     start_time =
       Timex.now("America/New_York")
-      |> Timex.beginning_of_day()
+      |> Timex.shift(hours: -1)
+      |> Timex.Timezone.convert("Etc/UTC")
 
     end_time =
       Timex.now("America/New_York")
-      |> Timex.end_of_day()
+      |> Timex.Timezone.convert("Etc/UTC")
 
-    data_points = WeatherMqtt.get_between_time(start_time, end_time)
+    {bmp_temps, sht_temps} =
+      WeatherMqtt.get_history_between(start_time, end_time)
+      |> format_results()
 
-    assign(socket, data: format_temp_data(data_points))
+    assign(socket, bmp_data: bmp_temps, sht_data: sht_temps)
   end
 
-  defp format_temp_data(data_points) when is_list(data_points) do
-    Enum.map(data_points, fn point ->
+  defp format_results(rows) do
+    bmp_temps = Enum.map(rows, fn row ->
       %{
-        x: Timex.to_datetime(point.time, "America/New_York") |> Timex.format!("{ISO:Extended}"),
-        y: point.bmp_temp
+        x: Timex.to_datetime(row.time, row.timezone) |> Timex.format!("{ISO:Extended}"),
+        y: row.bmp_temp
       }
     end)
+
+    sht_temps = Enum.map(rows, fn row ->
+      %{
+        x: Timex.to_datetime(row.time, row.timezone) |> Timex.format!("{ISO:Extended}"),
+        y: row.sht_temp
+      }
+    end)
+
+    {bmp_temps, sht_temps}
+  end
+
+  defp format_raw_results(rows) do
+    bmp_temps = Enum.map(rows, fn [bmp, _sht, timestamp] ->
+      %{
+        x: Timex.to_datetime(timestamp, "America/New_York") |> Timex.format!("{ISO:Extended}"),
+        y: Decimal.to_float(bmp)
+      }
+    end)
+
+    sht_temps = Enum.map(rows, fn [_bmp, sht, timestamp] ->
+      %{
+        x: Timex.to_datetime(timestamp, "America/New_York") |> Timex.format!("{ISO:Extended}"),
+        y: Decimal.to_float(sht)
+      }
+    end)
+
+    {bmp_temps, sht_temps}
   end
 end
